579d2312dce3fb575ab804491535d6c618be45e1
Ali Hamza <126351564+mrhamzasaghir@users.noreply.github.com>
2023-04-15 12:36:18 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,45 @@
+//Scrolling
+let nav = document.querySelector(".navbar");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 100){
+        nav.classList.add("header-scrolled");
+    }
+    else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+//select menu end
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            $('.filter').show('1000');
+        }
+        else
+        {
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+
+
+
